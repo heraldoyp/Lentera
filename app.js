2be8716a40f5277123a224bc9df9fbb0f76ade05
awtian <say@awtian.com>
2018-01-27 15:34:03 +0700

    add basic routing

--- app.js
+++ app.js
@@ -9,10 +9,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req,res) => models.Ideas.findAll().then(hehe => res.send(`<img src ='${hehe[0].image}'>`)))
+app.get('/', (req,res) => res.send(`<center><h1><a href = "/ideas"> ideas </a> <br><a href = "/user"> user </a></h1></center>`));
 app.use('/ideas', ideas);
 app.use('/user', user);
-
-
 
 app.listen(3000, () => console.log('Cek TKP di Port 3000 gan!!'))
--- routes/ideas.js
+++ routes/ideas.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const models = require('./models');
+const models = require('../models');
 
 router.get('/', (req, res) => {
   res.send('This is ideas page')
