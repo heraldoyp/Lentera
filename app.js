623633dea49e595b94e155186e17051484550ddb
awtian <say@awtian.com>
2018-01-27 15:25:16 +0700

    add basic routing

--- app.js
+++ app.js
@@ -2,20 +2,17 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
 const models = require('./models')
+const ideas = require('./routes/ideas.js')
+const user = require('./routes/user.js')
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req,res) => models.Ideas.findAll().then(hehe => res.send(`<img src ='${hehe[0].image}'>`)))
+app.use('/ideas', ideas);
+app.use('/user', user);
+
+
 
 app.listen(3000, () => console.log('Cek TKP di Port 3000 gan!!'))
-
-
-// req.body.imgst = "<img src ='"
-// req.body.img = url
-// req.body.imgen = "'>"
-
-// // {
-// //   image = 
-// // }
