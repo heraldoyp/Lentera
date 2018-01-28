7586d8015f6f289790d22b54175650bd7029aa0f
awtian <say@awtian.com>
2018-01-28 19:19:03 +0700

    MVC, sequelize, basic ejs done

--- routes/ideas.js
+++ routes/ideas.js
@@ -3,11 +3,42 @@ const router = express.Router();
 const models = require('../models');
 
 router.get('/', (req, res) => {
-  models.Ideas.findAll().then(result => res.render('ideas.ejs', datas=result))
+  models.Ideas.findAll({order : [['id', 'ASC']]}).then(result => res.render('ideas.ejs', datas=result))
 })
 
-router.get('/:id', (req, res) => {
+router.get('/view/:id', (req, res) => {
   models.Ideas.findById(req.params.id).then(data => res.render('ideasbyid.ejs', idea = data))
+})
+
+router.get('/edit/:id', (req, res) => {
+  models.Ideas.findById(req.params.id).then(data => res.render('editidea.ejs', idea = data))
+})
+
+router.post('/edit/:id', (req, res) => {
+  let newdata = {
+    overview : req.body.overview,
+    image : req.body.image,
+    option : req.body.option
+  }
+  models.Ideas.update(newdata, {where: {id: req.params.id}}).then(data => res.redirect('/ideas'))
+})
+
+router.get('/create', (req, res) => {
+  res.render('createideas.ejs')
+})
+
+router.post('/create', (req, res) => {
+  let newidea = {
+    overview : req.body.overview,
+    image : req.body.image,
+    option : req.body.option
+  }
+  models.Ideas.create(newidea).then(data => res.redirect('/ideas'))
+})
+
+
+router.get('/delete/:id', (req, res) => {
+  models.Ideas.destroy({where: {id: req.params.id}}).then(deleted => res.redirect('/ideas'))
 })
 
 
